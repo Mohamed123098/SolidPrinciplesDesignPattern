b19ababcde48c56cd4f09c41241b6843c48f3c24
Mohamed Ragab Hussein <85412361+Mohamed123098@users.noreply.github.com>
2026-03-01 17:07:25 +0200

    Add project files.

---
+++ SingleResponsibility/SingleResponsibility/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace SingleReposibility
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            Console.WriteLine(EmailModifier.ModifyEmail("Hello,Muhammad "));
+            Console.WriteLine(EmailSender.SendEmail("Hello,Muhammad"));
+        }
+    }
+}
