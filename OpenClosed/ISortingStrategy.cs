f1144f877ad2a9bd05b94ff8a7d348d64bb6053d
Mohamed Ragab Hussein <85412361+Mohamed123098@users.noreply.github.com>
2026-03-01 17:17:41 +0200

    Open for extension closed for modification

---
+++ OpenClosed/ISortingStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenForExtensionClosedForModification
+{
+    internal interface ISortingStrategy<T>
+    {
+        void Sorting(List<T> list);
+    }
+}
---
+++ OpenClosed/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace OpenForExtensionClosedForModification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> ints = [1, 8, -5, 0, 15, 18];
+            SortingAlgorithms<int>(new CustomSorting(), ints);
+            
+        }
+        public static void SortingAlgorithms<T>(ISortingStrategy<T> sorting,List<T> values)
+        {
+            sorting.Sorting(values);
+        }
+    }
+}
